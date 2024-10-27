04a8b1495e90b0772aba0332d628915f4e25ffe0
ejoaliaj <ejona.aliaj@eng.it>
2024-10-27 16:42:52 +0100

    Add existing project files prior to the push to GitHub.

---
+++ src/main/java/com/crispy/crispy_be_challenge_ejona_aliaj/ToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.crispy.crispy_be_challenge_ejona_aliaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crispy/crispy_be_challenge_ejona_aliaj/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crispy.crispy_be_challenge_ejona_aliaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
