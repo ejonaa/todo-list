9cb8b0dbb349d49451cd99641621cd420ff8a89e
ejoaliaj <ejona.aliaj@eng.it>
2024-10-29 17:28:23 +0100

    Add bean model mapper config
    - enable jpa auditing

--- src/main/java/com/crispy/crispy_be_challenge_ejona_aliaj/ToDoListApplication.java
+++ src/main/java/com/crispy/crispy_be_challenge_ejona_aliaj/ToDoListApplication.java
@@ -1,13 +1,23 @@
 package com.crispy.crispy_be_challenge_ejona_aliaj;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class ToDoListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToDoListApplication.class, args);
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApplication.class, args);
+    }
 
 }
