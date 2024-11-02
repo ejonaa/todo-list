b107b47390774243269aa87b90c5ab15467d2f6e
ejoaliaj <ejona.aliaj@eng.it>
2024-11-02 22:19:19 +0100

    Add tests for account creation

---
+++ src/test/java/com/crispy/crispy_be_challenge_ejona_aliaj/AccountControllerTests.java
@@ -0,0 +1,45 @@
+package com.crispy.crispy_be_challenge_ejona_aliaj;
+
+import com.crispy.crispy_be_challenge_ejona_aliaj.controller.request.security.NewUserRequest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class AccountControllerTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void shouldCreateANewUser() {
+        NewUserRequest newUserMock = createMockUser("test1");
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/api/create-account", newUserMock, Void.class);
+        Assertions.assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    @Test
+    void shouldNotCreateUserWithSameUsername() {
+        NewUserRequest newUserMock = createMockUser("test2");
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/api/create-account", newUserMock, Void.class);
+        Assertions.assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        ResponseEntity<Void> retryCreateResponse = restTemplate.postForEntity("/api/create-account", newUserMock, Void.class);
+        Assertions.assertThat(retryCreateResponse.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+    }
+
+    private static NewUserRequest createMockUser(String userName) {
+        NewUserRequest newUserMock = new NewUserRequest();
+        newUserMock.setFirstName("Test");
+        newUserMock.setLastName("Test");
+        newUserMock.setLogin(userName);
+        newUserMock.setPassword("test");
+        return newUserMock;
+    }
+}
